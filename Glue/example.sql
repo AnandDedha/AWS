a43b3b9cb1d67f9f4f4501d8851f026da1dd848c
Anand Kumar <ananddedha@gmail.com>
2023-07-03 17:35:02 -0400

    Create example.sql

---
+++ Glue/example.sql
@@ -0,0 +1,10 @@
+Select userid, name, gender, genderlooking, age,
+       Case when lang_fr =True then 'French'
+            when lang_en =True then 'English'
+            when lang_de =True then 'German'
+            when lang_it =True then 'Italian'
+            when lang_es =True then 'Hindi'
+            when lang_pt =True then 'Spanish'
+           else "Language missiong" end as Lang,
+     lastonlinetime
+from myDataSource
