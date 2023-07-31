6cc09c62dc65ac3554cce3e0824e111f4da6462b
Anand Kumar <ananddedha@gmail.com>
2023-07-31 19:20:51 -0400

    Create redshift-ddl.sql

---
+++ aws-etl/redshift-ddl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Regionalsales (
+    Region VARCHAR(50),
+    Country VARCHAR(50),
+    year Bigint,
+    Quarter INT,
+    Total_Revenue DECIMAL(15, 2),
+    Total_Cost DECIMAL(15, 2),
+    Total_Profit DECIMAL(15, 2)
+);
