140f55e2714ecea8bddd4ba50157b2c34f7ffaf2
Anand Kumar <ananddedha@gmail.com>
2023-08-13 18:22:32 -0400

    Update openweather_api.py

--- aws-airflow/dags/openweather_api.py
+++ aws-airflow/dags/openweather_api.py
@@ -32,7 +32,7 @@ def extract_openweather_data(**kwargs):
     response = requests.get(api_endpoint, params=api_params)
     data = response.json()
     print(data)
-    df= pd.json_normalize(data)
+    df= pd.json_normalize(data['list'])
     #df = pd.DataFrame(data['weather'])
     print(df)
     ti.xcom_push(key = 'final_data' , value = df.to_csv(index=False))
