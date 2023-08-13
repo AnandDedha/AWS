9fb3e50f0dc0045420d99ed053cc1251a13e3b08
Anand Kumar <ananddedha@gmail.com>
2023-08-12 21:19:36 -0400

    Update transform_redshift_load.py

--- aws-airflow/dags/transform_redshift_load.py
+++ aws-airflow/dags/transform_redshift_load.py
@@ -16,11 +16,11 @@ dag = DAG('s3_to_redshift_dag', default_args=default_args, schedule_interval=tim
 
 
 # Define the Glue Job
-glue_job_task = GlueJobOperator(
-    task_id='glue_job',
+transform_task = GlueJobOperator(
+    task_id='transform_task',
     job_name='your_glue_job_name',
-    script_location='s3://your-glue-scripts-bucket/your-glue-script.py',
-    aws_conn_id='aws_default',  # You'll need to set up an AWS connection in Airflow
+    script_location='s3://datatech-glue-scripts/your-glue-script.py',
+    aws_conn_id='AWS_CONN',  # You'll need to set up an AWS connection in Airflow
     region_name='your_aws_region',
     num_of_dpus=2,
     dag=dag,
@@ -35,7 +35,7 @@ s3_to_redshift_task = S3ToRedshiftOperator(
     schema_mapping=["column_name_1:VARCHAR", "column_name_2:INTEGER"],  # Define column mapping
     s3_bucket='your_s3_bucket',
     s3_key='your_s3_key',
-    aws_conn_id='aws_default',  # You'll need to set up an AWS connection in Airflow
+    aws_conn_id='AWS_CONN',  # You'll need to set up an AWS connection in Airflow
     redshift_conn_id='redshift_default',  # You'll need to set up a Redshift connection in Airflow
     dag=dag,
 )
@@ -43,4 +43,4 @@ s3_to_redshift_task = S3ToRedshiftOperator(
 
 
 # Set task dependencies
-glue_job_task >> s3_to_redshift_task 
+transform_task >> s3_to_redshift_task 
