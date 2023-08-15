4b5a25aa91724b93f7c989a9c07d3ff1e3cde700
Anand Kumar <ananddedha@gmail.com>
2023-08-15 14:12:58 -0400

    Update transform.py

--- aws-airflow/scripts/transform.py
+++ aws-airflow/scripts/transform.py
@@ -32,21 +32,21 @@ changeschema_weather_dyf = ApplyMapping.apply(
     mappings=[
         ("dt", "string", "dt", "string"),
         ("weather", "string", "weather", "string"),
+        ("visibility", "string", "visibility", "string"),
         ("`main.temp`", "string", "temp", "string"),
         ("`main.feels_like`", "string", "feels_like", "string"),
         ("`main.temp_min`", "string", "min_temp", "string"),
         ("`main.temp_max`", "string", "max_temp", "string"),
-        ("`main.pressure`", "string", "pressure", "bigint"),
-        ("`main.sea_level`", "string", "sea_level", "bigint"),
-        ("`main.grnd_level`", "string", "ground_level", "bigint"),
-        ("`main.humidity`", "string", "humidity", "bigint"),
+        ("`main.pressure`", "string", "pressure", "string"),
+        ("`main.sea_level`", "string", "sea_level", "string"),
+        ("`main.grnd_level`", "string", "ground_level", "string"),
+        ("`main.humidity`", "string", "humidity", "string"),
         ("`wind.speed`", "string", "wind", "string"),
     ],
     transformation_ctx="changeschema_weather_dyf",
 )
 
 #changeschema_weather_dyf.show()
-
 
 redshift_output = glueContext.write_dynamic_frame.from_options(
     frame=changeschema_weather_dyf,
@@ -57,7 +57,7 @@ redshift_output = glueContext.write_dynamic_frame.from_options(
         "aws_iam_role": "arn:aws:iam::262136919150:role/datapipeline-demo-RedshiftIamRole-1KQUQK6ZER2WH",
         "dbtable": "public.weather_data",
         "connectionName": "redshift-demo-connection",
-        "preactions": "DROP TABLE IF EXISTS public.weather_data; CREATE TABLE IF NOT EXISTS public.weather_data (dt VARCHAR, weather VARCHAR, temp VARCHAR, feels_like VARCHAR, min_temp VARCHAR, max_temp VARCHAR, pressure BIGINT, sea_level BIGINT, ground_level BIGINT, humidity BIGINT, wind VARCHAR);",
+        "preactions": "DROP TABLE IF EXISTS public.weather_data; CREATE TABLE IF NOT EXISTS public.weather_data (dt VARCHAR, weather VARCHAR, visibility VARCHAR, temp VARCHAR, feels_like VARCHAR, min_temp VARCHAR, max_temp VARCHAR, pressure VARCHAR, sea_level VARCHAR, ground_level VARCHAR, humidity VARCHAR, wind VARCHAR);",
     },
     transformation_ctx="redshift_output",
 )
